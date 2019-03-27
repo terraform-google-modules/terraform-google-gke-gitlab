0b24cd21577c06dac507fa80f5e505fd8922eeb4
Vic Iglesias <viglesias@google.com>
2019-03-27 21:58:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,170 @@
+# terraform-google-gke-gitlab
+
+This module was generated from [terraform-google-module-template](https://github.com/terraform-google-modules/terraform-google-module-template/), which by default generates a module that simply creates a GCS bucket. As the module develops, this README should be updated.
+
+The resources/services/activations/deletions that this module will create/trigger are:
+- Create a GCS bucket with the provided name
+
+## Usage
+There are examples included in the [examples](./examples/) folder but simple usage is as follows:
+
+```hcl
+module "bucket" {
+  source                     = "terraform-google-modules/gke-gitlab/google"
+  project_id                 = "<PROJECT ID>"
+  bucket_name                = "gcs-test-bucket"
+}
+```
+
+Then perform the following commands on the root folder:
+
+- `terraform init` to get the plugins
+- `terraform plan` to see the infrastructure plan
+- `terraform apply` to apply the infrastructure build
+- `terraform destroy` to destroy the built infrastructure
+
+
+[^]: (autogen_docs_start)
+
+[^]: (autogen_docs_end)
+
+## Requirements
+
+Before this module can be used on a project, you must ensure that the following pre-requisites are fulfilled:
+
+1. Terraform is [installed](#software-dependencies) on the machine where Terraform is executed.
+2. The Service Account you execute the module with has the right [permissions](#configure-a-service-account).
+3. The necessary APIs are [active](#enable-apis) on the project.
+
+The [project factory](https://github.com/terraform-google-modules/terraform-google-project-factory) can be used to provision projects with the correct APIs active.
+
+### Software Dependencies
+### Terraform
+- [Terraform](https://www.terraform.io/downloads.html) 0.10.x
+- [terraform-provider-google](https://github.com/terraform-providers/terraform-provider-google) plugin v1.8.0
+
+### Configure a Service Account
+In order to execute this module you must have a Service Account with the
+following project roles:
+- roles/storage.admin
+
+### Enable APIs
+In order to operate with the Service Account you must activate the following APIs on the project where the Service Account was created:
+
+- Storage JSON API - storage-api.googleapis.com
+
+## Install
+
+### Terraform
+Be sure you have the correct Terraform version (0.10.x), you can choose the binary here:
+- https://releases.hashicorp.com/terraform/
+
+## File structure
+The project has the following folders and files:
+
+- /: root folder
+- /examples: examples for using this module
+- /helpers: Helper scripts
+- /test: Folders with files for testing the module (see Testing section on this file)
+- /main.tf: main file for this module, contains all the resources to create
+- /variables.tf: all the variables for the module
+- /output.tf: the outputs of the module
+- /README.md: this file
+
+## Testing
+
+### Requirements
+- [bundler](https://github.com/bundler/bundler)
+- [gcloud](https://cloud.google.com/sdk/install)
+- [terraform-docs](https://github.com/segmentio/terraform-docs/releases) 0.3.0
+
+### Autogeneration of documentation from .tf files
+Run
+```
+make generate_docs
+```
+
+### Integration test
+
+Integration tests are run though [test-kitchen](https://github.com/test-kitchen/test-kitchen), [kitchen-terraform](https://github.com/newcontext-oss/kitchen-terraform), and [InSpec](https://github.com/inspec/inspec).
+
+`test-kitchen` instances are defined in [`.kitchen.yml`](./.kitchen.yml). The test-kitchen instances in `test/fixtures/` wrap identically-named examples in the `examples/` directory.
+
+#### Setup
+
+1. Configure the [test fixtures](#test-configuration)
+2. Download a Service Account key with the necessary permissions and put it in the module's root directory with the name `credentials.json`.
+3. Build the Docker container for testing:
+
+  ```
+  make docker_build_kitchen_terraform
+  ```
+4. Run the testing container in interactive mode:
+
+  ```
+  make docker_run
+  ```
+
+  The module root directory will be loaded into the Docker container at `/cft/workdir/`.
+5. Run kitchen-terraform to test the infrastructure:
+
+  1. `kitchen create` creates Terraform state and downloads modules, if applicable.
+  2. `kitchen converge` creates the underlying resources. Run `kitchen converge <INSTANCE_NAME>` to create resources for a specific test case.
+  3. `kitchen verify` tests the created infrastructure. Run `kitchen verify <INSTANCE_NAME>` to run a specific test case.
+  4. `kitchen destroy` tears down the underlying resources created by `kitchen converge`. Run `kitchen destroy <INSTANCE_NAME>` to tear down resources for a specific test case.
+
+Alternatively, you can simply run `make test_integration_docker` to run all the test steps non-interactively.
+
+#### Test configuration
+
+Each test-kitchen instance is configured with a `variables.tfvars` file in the test fixture directory. For convenience, since all of the variables are project-specific, these files have been symlinked to `test/fixtures/shared/terraform.tfvars`.
+Similarly, each test fixture has a `variables.tf` to define these variables, and an `outputs.tf` to facilitate providing necessary information for `inspec` to locate and query against created resources.
+
+Each test-kitchen instance creates necessary fixtures to house resources.
+
+### Autogeneration of documentation from .tf files
+Run
+```
+make generate_docs
+```
+
+### Linting
+The makefile in this project will lint or sometimes just format any shell,
+Python, golang, Terraform, or Dockerfiles. The linters will only be run if
+the makefile finds files with the appropriate file extension.
+
+All of the linter checks are in the default make target, so you just have to
+run
+
+```
+make -s
+```
+
+The -s is for 'silent'. Successful output looks like this
+
+```
+Running shellcheck
+Running flake8
+Running go fmt and go vet
+Running terraform validate
+Running hadolint on Dockerfiles
+Checking for required files
+Testing the validity of the header check
+..
+----------------------------------------------------------------------
+Ran 2 tests in 0.026s
+
+OK
+Checking file headers
+The following lines have trailing whitespace
+```
+
+The linters
+are as follows:
+* Shell - shellcheck. Can be found in homebrew
+* Python - flake8. Can be installed with 'pip install flake8'
+* Golang - gofmt. gofmt comes with the standard golang installation. golang
+is a compiled language so there is no standard linter.
+* Terraform - terraform has a built-in linter in the 'terraform validate'
+command.
+* Dockerfiles - hadolint. Can be found in homebrew
---
+++ main.tf
@@ -0,0 +1,374 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "project" {
+  description = "GCP Project to deploy resources"
+}
+
+variable "gitlab-db-password" {
+  description = "Password for the GitLab Postgres user"
+}
+
+variable "certmanager_email" {
+  description = "Email used to retrieve SSL certificates from Let's Encrypt"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+provider "google" {
+  project = "${var.project}"
+}
+
+provider "google-beta" {
+  project = "${var.project}"
+}
+
+provider "helm" {
+  service_account = "tiller"
+  install_tiller  = true
+  namespace       = "kube-system"
+
+  kubernetes {
+    host                   = "${google_container_cluster.gitlab.endpoint}"
+    client_certificate     = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_certificate)}"
+    client_key             = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_key)}"
+    cluster_ca_certificate = "${base64decode(google_container_cluster.gitlab.master_auth.0.cluster_ca_certificate)}"
+  }
+}
+
+provider "kubernetes" {
+  host                   = "${google_container_cluster.gitlab.endpoint}"
+  client_certificate     = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_certificate)}"
+  client_key             = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_key)}"
+  cluster_ca_certificate = "${base64decode(google_container_cluster.gitlab.master_auth.0.cluster_ca_certificate)}"
+}
+
+// IAM
+resource "google_project_service" "compute" {
+  project            = "${var.project}"
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "gke" {
+  project            = "${var.project}"
+  service            = "container.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "service_networking" {
+  project            = "${var.project}"
+  service            = "servicenetworking.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "cloudresourcemanager" {
+  project            = "${var.project}"
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "redis" {
+  project            = "${var.project}"
+  service            = "redis.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_service_account" "gitlab_gcs" {
+  project      = "${var.project}"
+  account_id   = "gitlab-gcs"
+  display_name = "GitLab Cloud Storage"
+}
+
+resource "google_service_account_key" "gitlab_gcs" {
+  service_account_id = "${google_service_account.gitlab_gcs.name}"
+}
+
+resource "google_project_iam_member" "project" {
+  project = "${var.project}"
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.gitlab_gcs.email}"
+}
+
+// Networking
+resource "google_compute_network" "gitlab" {
+  name                    = "gitlab"
+  project                 = "${var.project}"
+  auto_create_subnetworks = false
+  depends_on              = ["google_project_service.compute"]
+}
+
+resource "google_compute_subnetwork" "us-central" {
+  name          = "gitlab"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "${var.region}"
+  network       = "${google_compute_network.gitlab.self_link}"
+}
+
+resource "google_compute_address" "gitlab" {
+  name         = "gitlab"
+  region       = "${var.region}"
+  address_type = "EXTERNAL"
+  description  = "Gitlab Ingress IP"
+  depends_on              = ["google_project_service.compute"]
+}
+
+// Database
+resource "google_compute_global_address" "gitlab_sql" {
+  provider      = "google-beta"
+  project       = "${var.project}"
+  name          = "gitlab-sql"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  network       = "${google_compute_network.gitlab.self_link}"
+  prefix_length = 16
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  provider                = "google-beta"
+  network                 = "${google_compute_network.gitlab.self_link}"
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = ["${google_compute_global_address.gitlab_sql.name}"]
+}
+
+resource "google_sql_database_instance" "gitlab_db" {
+  depends_on       = ["google_service_networking_connection.private_vpc_connection"]
+  name             = "gitlab-db"
+  region           = "${var.region}"
+  database_version = "POSTGRES_9_6"
+
+  settings {
+    tier            = "db-custom-4-15360"
+    disk_autoresize = true
+
+    ip_configuration {
+      ipv4_enabled    = "false"
+      private_network = "${google_compute_network.gitlab.self_link}"
+    }
+  }
+}
+
+resource "google_sql_database" "gitlabhq_production" {
+  name     = "gitlabhq_production"
+  instance = "${google_sql_database_instance.gitlab_db.name}"
+}
+
+resource "google_sql_user" "gitlab" {
+  name     = "gitlab"
+  instance = "${google_sql_database_instance.gitlab_db.name}"
+  password = "${var.gitlab-db-password}"
+}
+
+// Redis
+resource "google_redis_instance" "gitlab" {
+  name           = "gitlab"
+  tier           = "STANDARD_HA"
+  memory_size_gb = 5
+  region         = "${var.region}"
+  authorized_network = "${google_compute_network.gitlab.self_link}"
+
+  depends_on     = ["google_project_service.redis"]
+
+  location_id             = "${var.region}-a"
+  alternative_location_id = "${var.region}-f"
+  display_name            = "GitLab Redis"
+}
+
+// Cloud Storage
+resource "google_storage_bucket" "gitlab-uploads" {
+  name     = "${var.project}-uploads"
+  location = "${var.region}"
+}
+
+resource "google_storage_bucket" "gitlab-artifacts" {
+  name     = "${var.project}-artifacts"
+  location = "${var.region}"
+}
+
+resource "google_storage_bucket" "gitlab-lfs" {
+  name     = "${var.project}-lfs"
+  location = "${var.region}"
+}
+
+resource "google_storage_bucket" "gitlab-packages" {
+  name     = "${var.project}-packages"
+  location = "${var.region}"
+}
+
+resource "google_storage_bucket" "gitlab-registry" {
+  name     = "${var.project}-registry"
+  location = "${var.region}"
+} 
+
+// GKE Cluster
+resource "google_container_cluster" "gitlab" {
+  project            = "${var.project}"
+  name               = "gitlab"
+  region             = "${var.region}"
+  min_master_version = "1.11"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+
+  initial_node_count = 1
+
+  network = "${google_compute_network.gitlab.self_link}"
+  subnetwork = "${google_compute_subnetwork.us-central.self_link}"
+  ip_allocation_policy {
+    # Allocate ranges automatically
+    cluster_ipv4_cidr_block  = ""
+    services_ipv4_cidr_block = ""
+  }
+
+  enable_legacy_abac = true
+
+  # Setting an empty username and password explicitly disables basic auth
+  master_auth {
+    username = ""
+    password = ""
+  }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+
+  depends_on = ["google_project_service.gke"]
+}
+
+resource "google_container_node_pool" "gitlab" {
+  name       = "gitlab"
+  region     = "${var.region}"
+  zone       = "${var.region}-a"
+  cluster    = "${google_container_cluster.gitlab.name}"
+  node_count = 1
+  depends_on = []
+
+  node_config {
+    preemptible  = false
+    machine_type = "n1-standard-4"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+
+resource "helm_repository" "gitlab" {
+  name = "gitlab"
+  url  = "https://charts.gitlab.io"
+}
+
+resource "kubernetes_service_account" "tiller" {
+  metadata {
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "tiller-admin" {
+  metadata {
+    name = "tiller-admin"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+}
+
+resource "kubernetes_secret" "gitlab_pg" {
+  metadata {
+    name = "gitlab-pg"
+  }
+
+  data {
+    password = "${var.gitlab-db-password}"
+  }
+}
+
+resource "kubernetes_secret" "gitlab_rails_storage" {
+  metadata {
+    name = "gitlab-rails-storage"
+  }
+
+  data {
+    connection = <<EOT
+provider: Google
+google_project: ${var.project}
+google_client_email: ${google_service_account.gitlab_gcs.email}
+google_json_key_string: '${base64decode(google_service_account_key.gitlab_gcs.private_key)}'
+EOT
+  }
+}
+
+resource "kubernetes_secret" "gitlab_gcs_credentials" {
+  metadata {
+    name = "google-application-credentials"
+  }
+
+  data {
+    gcs-application-credentials-file = "${base64decode(google_service_account_key.gitlab_gcs.private_key)}"
+  }
+}
+
+data "template_file" "helm_values" {
+  template = "${file("${path.module}/values.yaml.tpl")}"
+  vars = {
+    INGRESS_IP = "${google_compute_address.gitlab.address}"
+    DB_PRIVATE_IP = "${google_sql_database_instance.gitlab_db.private_ip_address}"
+    REDIS_PRIVATE_IP = "${google_redis_instance.gitlab.host}"
+    PROJECT_ID = "${var.project}"
+    CERT_MANAGER_EMAIL = "${var.certmanager_email}"
+  }
+}
+
+resource "helm_release" "gitlab" {
+  name       = "gitlab"
+  repository = "${helm_repository.gitlab.0.name}"
+  chart      = "gitlab"
+  version    = "1.7.1"
+  timeout    = 600
+
+
+  values = [ "${data.template_file.helm_values.rendered}" ]
+
+
+  depends_on = ["google_redis_instance.gitlab",
+    "google_sql_database.gitlabhq_production",
+    "google_sql_user.gitlab",
+    "kubernetes_cluster_role_binding.tiller-admin",
+  ]
+}
+
