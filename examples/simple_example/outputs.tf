a354a3af9ccb3ef76dd4ef94e3cd0da16a92cecc
Vic Iglesias <viglesias@google.com>
2019-03-27 23:14:25 +0000

    Add root password instructions to outputs

--- examples/simple_example/outputs.tf
+++ examples/simple_example/outputs.tf
@@ -17,3 +17,7 @@
 output "gitlab_url" {
   value = "${module.gke-gitlab.gitlab_url}"
 }
+
+output "root_password_instructions" {
+  value = "${module.gke-gitlab.root_password_instructions}"
+}
--- outputs.tf
+++ outputs.tf
@@ -18,3 +18,14 @@ output "gitlab_url" {
   value = "https://gitlab.${google_compute_address.gitlab.address}.xip.io"
   description = "URL where you can access your GitLab instance"
 }
+
+output "root_password_instructions" {
+  value = <<EOF
+
+  Run the following commands to get the root user password:
+
+  gcloud container clusters get-credentials gitlab --zone ${var.region} --project ${var.project_id}
+  kubectl get secret gitlab-gitlab-initial-root-password -o go-template='{{ .data.password }}' | base64 -d && echo
+  EOF
+  description = "Instructions for getting the root user's password for initial setup"
+}
