66ada216144caf4b027d35b90e09a5fca14ab98f
Mateusz Filipowicz <mateusz.filipowicz@roche.com>
2019-10-22 14:58:53 +0200

    removed duplicated project service gke

--- main.tf
+++ main.tf
@@ -28,21 +28,23 @@ provider "helm" {
   namespace       = "kube-system"
 
   kubernetes {
-    insecure               = true
-    host                   = "${google_container_cluster.gitlab.endpoint}"
-    client_certificate     = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_certificate)}"
-    client_key             = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_key)}"
-//    cluster_ca_certificate = "${base64decode(google_container_cluster.gitlab.master_auth.0.cluster_ca_certificate)}"
+    insecure           = true
+    host               = "${google_container_cluster.gitlab.endpoint}"
+    client_certificate = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_certificate)}"
+    client_key         = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_key)}"
+
+    //    cluster_ca_certificate = "${base64decode(google_container_cluster.gitlab.master_auth.0.cluster_ca_certificate)}"
   }
 }
 
 provider "kubernetes" {
-  insecure               = true
-  load_config_file       = false
-  host                   = "${google_container_cluster.gitlab.endpoint}"
-  client_certificate     = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_certificate)}"
-  client_key             = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_key)}"
-//  cluster_ca_certificate = "${base64decode(google_container_cluster.gitlab.master_auth.0.cluster_ca_certificate)}"
+  insecure           = true
+  load_config_file   = false
+  host               = "${google_container_cluster.gitlab.endpoint}"
+  client_certificate = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_certificate)}"
+  client_key         = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_key)}"
+
+  //  cluster_ca_certificate = "${base64decode(google_container_cluster.gitlab.master_auth.0.cluster_ca_certificate)}"
 }
 
 // IAM
@@ -52,11 +54,6 @@ resource "google_project_service" "compute" {
   disable_on_destroy = false
 }
 
-resource "google_project_service" "gke" {
-  project            = "${var.project_id}"
-  service            = "container.googleapis.com"
-  disable_on_destroy = false
-}
 
 resource "google_project_service" "service_networking" {
   project            = "${var.project_id}"
