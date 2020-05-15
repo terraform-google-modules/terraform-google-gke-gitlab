87b32fe9d957e11dfe69b7315adf606231ab29f8
Morgante Pell <morgantep@google.com>
2020-05-15 12:59:09 -0400

    Switch to using released auth module

--- main.tf
+++ main.tf
@@ -23,8 +23,8 @@ provider "google-beta" {
 }
 
 module "gke_auth" {
-  source  = "git::https://github.com/terraform-google-modules/terraform-google-kubernetes-engine.git//modules/auth?ref=fix/empty-auth"
-  # version = "~> 9.0"
+  source  = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  version = "~> 9.1"
 
   project_id       = var.project_id
   cluster_name     = module.gke.name
