76ed4c7d1c7cc9d4ebf002da960d36ba881144e0
Morgante Pell <morgantep@google.com>
2020-05-20 14:27:14 -0400

    Remove unused variable from example

--- examples/simple_example/variables.tf
+++ examples/simple_example/variables.tf
@@ -17,7 +17,3 @@
 variable "project_id" {
   description = "The project ID to deploy to"
 }
-
-variable "certmanager_email" {
-  description = "Email used to retrieve SSL certificates from Let's Encrypt"
-}
