1d9598730cda52069680be19e5eea995437226c9
Will Beebe <wbeebe@google.com>
2020-06-18 17:33:56 -0700

    trying diff provider

--- version.tf
+++ version.tf
@@ -30,7 +30,7 @@ terraform {
       version = "~> 0.10"
     }
     kubernetes = {
-      source  = "hashicorp/kubernetes"
+      source  = "terraform-providers/kubernetes"
       version = "~> 1.10.0"
     }
     null = {
