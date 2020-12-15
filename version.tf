44d654d29ae6ce474f559fcb34d1bd20ecc9426f
Ben Good <benjamingood@google.com>
2020-12-15 09:42:01 -0700

    Bumping provider versions to address issue found in downstream project.

--- version.tf
+++ version.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.39"
+      version = "~> 3.42"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 3.39"
+      version = "~> 3.42"
     }
     helm = {
       source  = "hashicorp/helm"
