903c06dce4f0aee28c2bee81e73f7500958f5964
Andrew Peabody <andrewpeabody@google.com>
2022-12-29 09:51:48 -0800

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -35,7 +35,7 @@ terraform {
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.2.0"
+      version = ">= 2.1.2"
     }
     random = {
       source  = "hashicorp/random"
