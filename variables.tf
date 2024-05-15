b32a1930d2134d758a59ad08e18214ef6a888acc
Andrew Peabody <andrewpeabody@google.com>
2024-05-15 13:36:41 -0700

    [DO NOT MERGE]

--- variables.tf
+++ variables.tf
@@ -33,7 +33,7 @@ variable "certmanager_email" {
 variable "gke_version" {
   description = "Version of GKE to use for the GitLab cluster"
   type        = string
-  default     = "1.21"
+  default     = "1.26"
 }
 
 variable "gke_machine_type" {
